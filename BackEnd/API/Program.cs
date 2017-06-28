a9d2f9853e2183d047adf634eb73d71c75c68bb9
Lê Quang Vinh <lqv_127604@yahoo.co.uk>
2017-06-28 18:04:34 +0700

    Commit: thư viện cho BackEnd

--- BackEnd/API/Program.cs
+++ BackEnd/API/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace API
 {
