283ff954b3aebd4bf75f476b069b8e55bce8add6
Lê Quang Vinh <lqv_127604@yahoo.co.uk>
2017-07-03 14:42:42 +0700

    Kiến trúc Củ hành: - CoreCI: dùng chung cho cả WinForm (SysAdmin) và API, CoreFX - CoreFX: tầng ngoài của CoreCI - API: tầng bao ngoài cùng của CoreFX - WinForm: độc lập với API

--- BackEnd/API/Startup.cs
+++ BackEnd/API/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace API
 {
@@ -15,10 +18,8 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- BackEnd/SysAdmin/Properties/AssemblyInfo.cs
+++ BackEnd/SysAdmin/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d2323aa6-8f37-49c5-968b-a418c4525291")]
+[assembly: Guid("9208b9e9-e796-4836-879a-175cd9468d5d")]
 
 // Version information for an assembly consists of the following four values:
 //
